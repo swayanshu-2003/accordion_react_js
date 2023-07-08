bcc6dcd3c63d4a128f3150e9cf5903e169adb487
Swayanshu Sekhar <62717967+swayanshu-2003@users.noreply.github.com>
2023-07-08 18:38:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import Heading from "./components/Heading";
+
+
+const App = () => {
+  return (
+    <>
+      <Heading/>
+      <Accordion />
+    </>
+  );
+};
+
+export default App;
